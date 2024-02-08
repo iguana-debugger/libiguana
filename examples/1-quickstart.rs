27288ed4552c949813f08f7a36f755c96b1942cb
James Harvey <44349936+jmshrv@users.noreply.github.com>
2024-02-08 20:39:02 +0000

    Nuke Unicorn-based implementation

--- examples/1-quickstart.rs
+++ examples/1-quickstart.rs
@@ -1,11 +1,7 @@
 use libiguana::Environment;
-use unicorn_engine::unicorn_const::{Arch, Mode};
 
 fn main() {
     let kmd = include_str!("hello.kmd");
 
-    let mut env =
-        Environment::new(Arch::ARM, Mode::LITTLE_ENDIAN).expect("Unable to setup environment!");
-
-    env.load_kmd(kmd);
+    let env = Environment::new().expect("Unable to setup environment!");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,91 +1,23 @@
-use std::process::exit;
-use std::str;
-
-use kmdparse::parse_kmd;
-use kmdparse::token::Token;
-use unicorn_engine::{
-    unicorn_const::{uc_error, Arch, Mode, Permission},
-    RegisterARM, Unicorn,
+use std::{
+    io,
+    process::{Child, Command, Stdio},
 };
 
-/// An environment for running CPU instructions in. Think of this as your "virtual CPU".
-pub struct Environment<'a> {
-    unicorn: Unicorn<'a, ()>,
+pub struct Environment {
+    jimulator_process: Child,
 }
 
-impl<'a> Environment<'a> {
-    /// Creates a new environment for the given architecture/mode. You probably want [Arch::ARM] and
-    /// [Mode::LITTLE_ENDIAN].
-    pub fn new(arch: Arch, mode: Mode) -> Result<Environment<'a>, uc_error> {
-        let unicorn = Unicorn::new(arch, mode)?;
+impl Environment {
+    pub fn new() -> Result<Self, io::Error> {
+        let jimulator_process = Command::new("jimulator").stdin(Stdio::piped()).spawn()?;
 
-        Ok(Environment { unicorn })
+        Ok(Self { jimulator_process })
     }
+}
 
-    pub fn load_kmd(&mut self, kmd: &str) {
-        let (_, parsed_kmd) = parse_kmd(kmd).unwrap();
-
-        self.unicorn
-            .mem_map(0x0, 1024 * 1024, Permission::ALL)
-            .unwrap();
-
-        for token in parsed_kmd {
-            if let Token::Line(line) = token {
-                if let Some(word) = line.word {
-                    self.unicorn
-                        .mem_write(line.memory_address as u64, &word.to_le_bytes())
-                        .unwrap();
-                }
-            }
-        }
-
-        self.unicorn
-            .add_intr_hook(|uc, _| {
-                let pc = uc.pc_read().unwrap() - 4;
-                let instruction = uc.mem_read_as_vec(pc, 4).unwrap();
-                if let Some(interrupt_num) = instruction.first() {
-                    println!("SWI {interrupt_num} called!");
-
-                    match interrupt_num {
-                        1 => todo!(),
-                        2 => exit(0),
-                        3 => {
-                            let address = uc.reg_read(RegisterARM::R0).expect("Failed to read R0!");
-
-                            let memory = uc
-                                .mem_read_as_vec(address, (1024 * 1024 - address) as usize)
-                                .expect("Failed to read memory!");
-
-                            let string_bytes = memory
-                                .chunks(4)
-                                .into_iter()
-                                .flat_map(|chunk| chunk.into_iter().rev())
-                                .take_while(|byte| **byte != 0)
-                                .map(|borrow| *borrow)
-                                .collect::<Vec<_>>();
-
-                            for byte in &string_bytes {
-                                print!("{:02X} ", byte);
-                            }
-
-                            println!();
-
-                            Self::print_str(&string_bytes).expect("Failed to print_str!")
-                        }
-                        _ => panic!("Invalid SWI: {interrupt_num}"),
-                    }
-                }
-            })
-            .unwrap();
-
-        self.unicorn.emu_start(0, 1024 * 1024, 0, 100).unwrap();
-    }
-
-    fn print_str(string_bytes: &[u8]) -> Result<(), uc_error> {
-        let string = str::from_utf8(&string_bytes).unwrap();
-
-        println!("{string}");
-
-        Ok(())
+impl Drop for Environment {
+    fn drop(&mut self) {
+        // We should probably check for errors here?
+        let _ = self.jimulator_process.kill();
     }
 }
