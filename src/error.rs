19aaabe1c5858d3e3bf3a454cf48d7ede2ac75ee
James Harvey <44349936+jmshrv@users.noreply.github.com>
2024-02-09 12:51:31 +0000

    Use thiserror for LibiguanaError

--- src/error.rs
+++ src/error.rs
@@ -1,12 +1,24 @@
-use std::{io, str};
+use std::{io, num::TryFromIntError, str};
+use thiserror::Error;
 
-#[derive(Debug)]
+#[derive(Debug, Error)]
 pub enum LibiguanaError {
+    #[error("jimulator process has no stdin")]
     NoStdin,
+
+    #[error("jimulator process has no stdout")]
     NoStdout,
-    IO(io::Error),
-    Utf8Error(str::Utf8Error),
+
+    #[error("An IO error occured when reading/writing to jimulator")]
+    IO(#[from] io::Error),
+
+    #[error("jimulator returned a string that was not valid UTF-8")]
+    Utf8Error(#[from] str::Utf8Error),
 
     // TODO: Give error information on parse errors
+    #[error("The given kmd file failed to parse")]
     ParseError,
+
+    #[error("An integer overflow occured")]
+    IntegerOverflow(#[from] TryFromIntError),
 }
--- src/lib.rs
+++ src/lib.rs
@@ -23,30 +23,6 @@ impl Environment {
         Ok(Self { jimulator_process })
     }
 
-    pub fn ping(&mut self) -> Result<String, LibiguanaError> {
-        self.jimulator_process
-            .stdin
-            .as_ref()
-            .ok_or(LibiguanaError::NoStdin)?
-            .write(&[0b0000_0001])
-            .map_err(|e| LibiguanaError::IO(e))?;
-
-        let mut buf = [0; 4];
-
-        self.jimulator_process
-            .stdout
-            .as_mut()
-            .ok_or(LibiguanaError::NoStdout)?
-            .read_exact(&mut buf)
-            .map_err(|e| LibiguanaError::IO(e))?;
-
-        let response = str::from_utf8(&buf)
-            .map_err(|e| LibiguanaError::Utf8Error(e))?
-            .to_string();
-
-        Ok(response)
-    }
-
     /// Loads the given .kmd file. [`kmd`] is an unparsed string - parsing is handled by this
     /// function.
     pub fn load_kmd(&mut self, kmd: &str) -> Result<(), LibiguanaError> {
@@ -55,7 +31,7 @@ impl Environment {
         for token in parsed {
             if let Token::Line(line) = token {
                 if let Some(word) = line.word {
-                    self.write_memory(word, line.memory_address)?;
+                    self.write_memory(&word, line.memory_address)?;
                 }
             }
         }
@@ -63,31 +39,45 @@ impl Environment {
         Ok(())
     }
 
-    fn write_memory(&mut self, word: u32, address: u32) -> Result<(), LibiguanaError> {
+    pub fn ping(&mut self) -> Result<String, LibiguanaError> {
+        self.jimulator_process
+            .stdin
+            .as_ref()
+            .ok_or(LibiguanaError::NoStdin)?
+            .write(&[0b0000_0001])?;
+
+        let mut buf = [0; 4];
+
+        self.jimulator_process
+            .stdout
+            .as_mut()
+            .ok_or(LibiguanaError::NoStdout)?
+            .read_exact(&mut buf)?;
+
+        let response = str::from_utf8(&buf)?.to_string();
+
+        Ok(response)
+    }
+
+    fn write_memory(&mut self, word: &[u8], address: u32) -> Result<(), LibiguanaError> {
         let mut stdin = self
             .jimulator_process
             .stdin
             .as_ref()
             .ok_or(LibiguanaError::NoStdin)?;
 
-        // Write memory transfer command (mem space, write, 32 bit)
-        stdin
-            .write(&[0b01_00_0_010])
-            .map_err(|e| LibiguanaError::IO(e))?;
+        // Write memory transfer command (mem space, write, 8 bit)
+        stdin.write(&[0b01_00_0_000])?;
 
         // Write address
-        stdin
-            .write(&address.to_le_bytes())
-            .map_err(|e| LibiguanaError::IO(e))?;
+        stdin.write(&address.to_le_bytes())?;
 
-        // Write number of elements (always 1 in this case)
-        stdin
-            .write(&1_u16.to_le_bytes())
-            .map_err(|e| LibiguanaError::IO(e))?;
+        let num_elements: u16 = word.len().try_into()?;
 
-        stdin
-            .write(&word.to_le_bytes())
-            .map_err(|e| LibiguanaError::IO(e))?;
+        // Write number of elements (number of elements in address slice)
+        stdin.write(&num_elements.to_le_bytes())?;
+
+        stdin.write(word)?;
 
         Ok(())
     }
