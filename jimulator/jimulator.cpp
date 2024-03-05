280a081e7cadfc693057d1ad4b8b3a7a8642d317
James Harvey <44349936+jmshrv@users.noreply.github.com>
2024-03-05 17:44:28 +0000

    Modify jimulator to die on parent death

--- jimulator/jimulator.cpp
+++ jimulator/jimulator.cpp
@@ -406,6 +406,13 @@ int main(int argc, char** argv) {
   }
 
   while (true) {
+    // If there is no parent process, exit. This stops MacIguana from leaving
+    // orphaned jimulators about taking up 100% CPU.
+    // https://stackoverflow.com/a/2035683/8532605
+    if (getppid() == 1) {
+      exit(0);
+    }
+
     comm(&pollfd);  // Check for monitor command
     if ((status & CLIENT_STATE_CLASS_MASK) == CLIENT_STATE_CLASS_RUNNING) {
       step();  // Step emulator as required
