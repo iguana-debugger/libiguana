3ce65208a75c3121654f5322c8901ab2a018c5da
James Harvey <44349936+jmshrv@users.noreply.github.com>
2024-03-22 16:41:47 +0000

    Set process group for jimulator

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use std::{
     array::TryFromSliceError,
     io::{Read, Write},
+    os::unix::process::CommandExt,
     path::Path,
     process::{Child, Command, Stdio},
     str,
@@ -66,6 +67,7 @@ impl IguanaEnvironment {
         let jimulator_process = Command::new(jimulator_path)
             .stdin(Stdio::piped())
             .stdout(Stdio::piped())
+            .process_group(0)
             .spawn()?;
 
         let jimulator_arc_mutex = Arc::new(Mutex::new(jimulator_process));
